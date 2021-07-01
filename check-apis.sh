70ee9c72395005c2b27e75633d698b36ec5cf1db
Jay Lee <jay0lee@gmail.com>
2021-07-01 08:59:57 -0400

    record URL doc was retrieved from

--- check-apis.sh
+++ check-apis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pyparser="import json, sys, collections; disc = json.loads(sys.stdin.read()); disc.pop('etag', None); disc.pop('revision', None); print(json.dumps(disc, indent=2, sort_keys=True))"
+pyparser="import json, os, sys, collections; disc = json.loads(sys.stdin.read()); disc.pop('etag', None); disc.pop('revision', None); disc['retrieved_from'] = os.getenv('use_url'); print(json.dumps(disc, indent=2, sort_keys=True))"
 
 while read line; do
     api=$(echo $line | cut -d' ' -f1);
@@ -9,10 +9,12 @@ while read line; do
     v2_url="https://${api}.googleapis.com/\$discovery/rest?version=${ver}";
     v1_url="https://www.googleapis.com/discovery/v1/apis/${api}/${ver}/rest";
     echo "Checking v2 URL for $api $ver...";
-    curl -s -f --compressed -o ~/$apifile $v2_url; result=$?; true;
+    use_url=$v2_url
+    curl -s -f --compressed -o ~/$apifile $use_url; result=$?; true;
     if [ $result -ne 0 ]; then
         echo "v2 URL failed with $result, checking v1 URL for $api $ver...";
-        curl -s --compressed -f -o ~/$apifile $v1_url; result=$?; true;
+        use_url=$v1_url
+        curl -s --compressed -f -o ~/$apifile $use_url; result=$?; true;
         if [ $result -ne 0 ]; then
             echo "$api $ver v1 failed also with $result!";
         fi;
