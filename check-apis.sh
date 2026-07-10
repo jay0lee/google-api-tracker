a41c9d4f6cb4425e6933b9bb0f815bffed8fc8fe
Jay Lee <jay0lee@gmail.com>
2026-07-10 07:20:25 -0400

    Refactor API key handling and improve script structure
    Refactored the Python script for better readability and added checks for API URLs without keys.

--- check-apis.sh
+++ check-apis.sh
@@ -1,25 +1,72 @@
 #!/bin/bash
 
-# Updated pyparser to redact the API key using regex
-pyparser="import json, os, sys, re; disc = json.loads(sys.stdin.read()); disc.pop('etag', None); disc.pop('revision', None); url = os.getenv('use_url', ''); disc['retrieved_from'] = re.sub(r'key=[^&]*', 'key=****', url); print(json.dumps(disc, indent=2, sort_keys=True))"
+# Define the multi-line Python script cleanly
+pyparser='
+import json
+import os
+import sys
+import re
+
+# Read JSON from standard input
+disc = json.loads(sys.stdin.read())
+
+# Remove volatile fields
+disc.pop("etag", None)
+disc.pop("revision", None)
+
+# Redact the API key using regex
+url = os.getenv("use_url", "")
+disc["retrieved_from"] = re.sub(r"key=[^&]*", "key=****", url)
+
+# Output the formatted JSON
+print(json.dumps(disc, indent=2, sort_keys=True))
+'
 
 while read line; do
     api=$(echo $line | cut -d' ' -f1);
     ver=$(echo $line | cut -d' ' -f2);
     apifile="${api}-${ver}.json"
-    v2_url="https://${api}.googleapis.com/\$discovery/rest?version=${ver}&key=${googleapikey}";
-    v1_url="https://www.googleapis.com/discovery/v1/apis/${api}/${ver}/rest?key=${googleapikey}";
-    echo "Checking v2 URL ${v2_url} for $api $ver...";
-    export use_url="${v2_url}"
+    
+    # 1) v2 URL with key
+    v2_url_key="https://${api}.googleapis.com/\$discovery/rest?version=${ver}&key=${googleapikey}"
+    # 2) v1 URL with key
+    v1_url_key="https://www.googleapis.com/discovery/v1/apis/${api}/${ver}/rest?key=${googleapikey}"
+    # 3) v2 URL without key
+    v2_url_nokey="https://${api}.googleapis.com/\$discovery/rest?version=${ver}"
+    # 4) v1 URL without key
+    v1_url_nokey="https://www.googleapis.com/discovery/v1/apis/${api}/${ver}/rest"
+    
+    echo "1) Checking v2 URL with key for $api $ver..."
+    export use_url="${v2_url_key}"
     curl -s -f --compressed -o ~/$apifile $use_url; result=$?; true;
+    
     if [ $result -ne 0 ]; then
-        echo " v2 URL failed with ${result}, checking v1 URL ${v1_url} for ${api} ${ver}...";
-        export use_url="${v1_url}"
-        curl -s --compressed -f -o ~/$apifile $use_url; result=$?; true;
+        echo "   v2 URL with key failed (${result})."
+        echo "2) Checking v1 URL with key for $api $ver..."
+        export use_url="${v1_url_key}"
+        curl -s -f --compressed -o ~/$apifile $use_url; result=$?; true;
+        
         if [ $result -ne 0 ]; then
-            echo " v1 also failed with ${result} for ${api} ${ver}. Giving up.";
-            continue
-        fi;
-    fi;
+            echo "   v1 URL with key failed (${result})."
+            echo "3) Checking v2 URL without key for $api $ver..."
+            export use_url="${v2_url_nokey}"
+            curl -s -f --compressed -o ~/$apifile $use_url; result=$?; true;
+            
+            if [ $result -ne 0 ]; then
+                echo "   v2 URL without key failed (${result})."
+                echo "4) Checking v1 URL without key for $api $ver..."
+                export use_url="${v1_url_nokey}"
+                curl -s -f --compressed -o ~/$apifile $use_url; result=$?; true;
+                
+                if [ $result -ne 0 ]; then
+                    echo "   v1 URL without key also failed (${result}). Giving up."
+                    continue
+                fi
+            fi
+        fi
+    fi
+    
+    # Process the successful JSON payload through the formatted Python script
     cat ~/$apifile | python3 -c "${pyparser}" > $apifile;
+
 done < apis.txt
